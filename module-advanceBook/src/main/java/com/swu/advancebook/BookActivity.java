1e33f1d3940bce15a7079df2922ddce064e92df7
zhangjun2 <zhangjun2@sensetime.com>
2021-11-26 12:52:08 +0800

    config module gradle (init)

---
+++ module-advanceBook/src/main/java/com/swu/advancebook/BookActivity.java
@@ -0,0 +1,14 @@
+package com.swu.advancebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
