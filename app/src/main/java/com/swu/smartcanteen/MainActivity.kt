34f294ddea06121cb3757d358bb70cabde3c74a9
as-didiwei <1640159170@qq.com>
2021-11-27 00:04:03 +0800

    add
    添加了Splash页面，同时添加了ids，anim等一些资源文件

--- app/src/main/java/com/swu/smartcanteen/MainActivity.kt
+++ app/src/main/java/com/swu/smartcanteen/MainActivity.kt
@@ -5,7 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //到这个页面的时候是top_in效果，即将要消失的Activity，使用silent效果
+        //overridePendingTransition(R.anim.top_in, R.anim.silent)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
