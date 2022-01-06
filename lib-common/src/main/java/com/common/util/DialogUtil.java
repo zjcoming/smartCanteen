da46f56c4cf94774d1836cdca67b622bbf97447f
as-didiwei <1640159170@qq.com>
2022-01-06 16:21:49 +0800

    对dialog工具类做了修改

--- lib-common/src/main/java/com/common/util/DialogUtil.java
+++ lib-common/src/main/java/com/common/util/DialogUtil.java
@@ -3,6 +3,7 @@ package com.common.util;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.KeyEvent;
 
 import com.common.constants.BaseAppConstants;
 
@@ -11,16 +12,28 @@ import com.common.constants.BaseAppConstants;
  * desc: 展示各种Dialog的工具类
  */
 public class DialogUtil {
+    /**
+     * 只有一个按钮的dialog
+     */
     public static void showOneBtnDialog(Context context,String title,String content,String btnName,DialogInterface.OnClickListener onClickListener){
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(title);
         builder.setMessage(content);
         builder.setPositiveButton(btnName,onClickListener);
         AlertDialog dialog = builder.create();
-        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCanceledOnTouchOutside(false);//点击外面 dialog不能消失
+        dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+                return true;
+            }
+        });
         dialog.show();
     }
-    //默认另外一个是取消
+
+    /**
+     * 有两个按钮的dialog，另外一个默认是“取消”键，点击后dialog消失
+     */
     public static void showTwoBtnDialog(Context context,String title,String content,String firstBtnName,DialogInterface.OnClickListener onClickListener){
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(title);
@@ -37,7 +50,9 @@ public class DialogUtil {
         dialog.show();
     }
 
-    //另外一个btn自己实现
+    /**
+     * 有两个按钮的dialog，两个按钮都自己实现
+     */
     public static void selfShowTwoBtnDialog(Context context,String title,String content,String firstBtnName,String secondBtnName,DialogInterface.OnClickListener firstBtn,DialogInterface.OnClickListener secondBtn){
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(title);
@@ -46,6 +61,12 @@ public class DialogUtil {
         builder.setNegativeButton(secondBtnName,secondBtn);
         AlertDialog dialog = builder.create();
         dialog.setCanceledOnTouchOutside(false);
+        dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+                return true;
+            }
+        });
         dialog.show();
     }
 }
