b82e69ffbbd1823e0098e0e67ac76c3b304ba471
as-didiwei <1640159170@qq.com>
2022-01-13 10:59:46 +0800

    新增loadingDialog

--- module_order/src/main/java/com/swu/module_order/ChooseFoodActivity.kt
+++ module_order/src/main/java/com/swu/module_order/ChooseFoodActivity.kt
@@ -4,13 +4,14 @@ import com.base.BaseActivity
 import com.common.util.FragmentUtil
 import com.swu.module_order.databinding.ActivityChooseFoodBinding
 import com.swu.module_order.fragment.ChooseFoodFragment
+import com.swu.module_order.fragment.ConfirmOrderFragment
 
 class ChooseFoodActivity : BaseActivity<ActivityChooseFoodBinding>() {
 
     override fun initData() {
         FragmentUtil.getInstance().startFragment(
             this,
-            ChooseFoodFragment(this).apply {
+            ConfirmOrderFragment(this).apply {
                 setJumpFragmentCallBack { fragment, anim ->
                     FragmentUtil.getInstance().startFragment(
                         requireActivity(), fragment, R.id.container,
--- module_order/src/main/java/com/swu/module_order/fragment/ConfirmOrderFragment.java
+++ module_order/src/main/java/com/swu/module_order/fragment/ConfirmOrderFragment.java
@@ -1,6 +1,7 @@
 package com.swu.module_order.fragment;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,13 +26,17 @@ import java.util.ArrayList;
  * desc: 确定订单页面
  */
 public class ConfirmOrderFragment extends BaseFragment<FragmentConfirmOrderBinding> implements View.OnClickListener {
+    public ConfirmOrderFragment(Context context){
 
+    }
 
     @Override
     public void initViews() {
         //初始化商品列表
         initGoods();
         getBinding().confirmOrderBuymode.setOnClickListener(this);
+
+
     }
     public void initGoods(){
         ArrayList<String> mDatas = new ArrayList<>();
@@ -54,7 +59,7 @@ public class ConfirmOrderFragment extends BaseFragment<FragmentConfirmOrderBindi
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.confirm_order_buymode:
-                clickBuyMode();
+                //clickBuyMode();
                 break;
             default:
                 break;
